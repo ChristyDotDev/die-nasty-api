7647b2e2bff43ae6ba0383496a0597a9c38fb85d
Christopher Campbell <christytc10@gmail.com>
2021-05-13 09:41:16 +0100

    return basic info

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return jsonify({'league': os.getenv("LEAGUE_ID"),
+                    'status': 'up'})
 
 
 if __name__ == '__main__':
